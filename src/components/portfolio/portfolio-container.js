868d769e6386841078ea96fd318393c910318a79
beloca <beloca_4@hotmail.com>
2023-11-20 16:51:33 +0100

    data filter

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,40 +1,55 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        this.state={
-            pageTitle:"Bienvenidos a mi portfolio",
-            data: [
-                {title:"Empresa 1"},
-                {title: "Empresa 2"},
-                {title: "Empresa 3"}
-            ]
-        };
-        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
-    }
-    PortfolioItems(){
-        return this.state.data.map(item=>{
-            return <PortfolioItem title={item.title} url={"google.com"}/>;
-        });
-    }
-    handlePageTitleUpdate() {
-        this.setState({
-          pageTitle: "Something Else"
-        });
-      }
-    render(){
-        return(
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-                {this.PortfolioItems()}
-                
-                <hr />
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      data: [
+        { title: "Quip", category: "eCommerce" },
+        { title: "Eventbrite", category: "Scheduling" },
+        { title: "Ministry Safe", category: "Enterprise" },
+        { title: "SwingAway", category: "eCommerce" }
+      ]
+    };
 
-                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
-            </div>
-        );
-    }
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
+
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
+
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
