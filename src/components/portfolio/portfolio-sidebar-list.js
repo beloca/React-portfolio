b0868ef39a15024ef93d9a3b0190fefbaae2e39e
beloca <beloca_4@hotmail.com>
2023-11-29 09:26:14 +0100

    building edit click handler

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,133 +1,38 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from "axios";
-import { library } from "@fortawesome/fontawesome-svg-core";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
-import NavigationContainer from "./navigation/navigation-container";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
-import Blog from "./pages/blog";
-import PortfolioManager from "./pages/portfolio-manager";
-import PortfolioDetail from "./portfolio/portfolio-detail";
-import Auth from "./pages/auth";
-import NoMatch from "./pages/no-match";
-
-library.add(faTrash, faSignOutAlt);
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loggedInStatus: "NOT_LOGGED_IN"
-    };
-
-    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
-    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
-    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
-  }
-
-  handleSuccessfulLogin() {
-    this.setState({
-      loggedInStatus: "LOGGED_IN"
-    });
-  }
-
-  handleUnsuccessfulLogin() {
-    this.setState({
-      loggedInStatus: "NOT_LOGGED_IN"
-    });
-  }
-
-  handleSuccessfulLogout() {
-    this.setState({
-      loggedInStatus: "NOT_LOGGED_IN"
-    });
-  }
-
-  checkLoginStatus() {
-    return axios
-      .get("https://api.devcamp.space/logged_in", {
-        withCredentials: true
-      })
-      .then(response => {
-        const loggedIn = response.data.logged_in;
-        const loggedInStatus = this.state.loggedInStatus;
-
-        if (loggedIn && loggedInStatus === "LOGGED_IN") {
-          return loggedIn;
-        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
-          this.setState({
-            loggedInStatus: "LOGGED_IN"
-          });
-        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
-          this.setState({
-            loggedInStatus: "NOT_LOGGED_IN"
-          });
-        }
-      })
-      .catch(error => {
-        console.log("Error", error);
-      });
-  }
-
-  componentDidMount() {
-    this.checkLoginStatus();
-  }
-
-  authorizedPages() {
-    return [
-      <Route
-        key="portfolio-manager"
-        path="/portfolio-manager"
-        component={PortfolioManager}
-      />
-    ];
-  }
-
-  render() {
+const PortfolioSidebarList = props => {
+  const portfolioList = props.data.map(portfolioItem => {
     return (
-      <div className="container">
-        <Router>
-          <div>
-            <NavigationContainer
-              loggedInStatus={this.state.loggedInStatus}
-              handleSuccessfulLogout={this.handleSuccessfulLogout}
-            />
+      <div key={portfolioItem.id} className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
+          <img src={portfolioItem.thumb_image_url} />
+        </div>
 
-            <Switch>
-              <Route exact path="/" component={Home} />
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
 
-              <Route
-                path="/auth"
-                render={props => (
-                  <Auth
-                    {...props}
-                    handleSuccessfulLogin={this.handleSuccessfulLogin}
-                    handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
-                  />
-                )}
-              />
+          <div className="actions">
+            <a
+              className="action-icon"
+              onClick={() => props.handleEditClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="edit" />
+            </a>
 
-              <Route path="/about-me" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
-              {this.state.loggedInStatus === "LOGGED_IN" ? (
-                this.authorizedPages()
-              ) : null}
-              <Route
-                exact
-                path="/portfolio/:slug"
-                component={PortfolioDetail}
-              />
-              <Route component={NoMatch} />
-            </Switch>
+            <a
+              className="action-icon"
+              onClick={() => props.handleDeleteClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="trash" />
+            </a>
           </div>
-        </Router>
+        </div>
       </div>
     );
-  }
-}
+  });
+
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
+};
+
+export default PortfolioSidebarList;
