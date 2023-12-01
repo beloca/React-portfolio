ec4a7698ee9006a4b4229be9cb5e570941f4a816
beloca <beloca_4@hotmail.com>
2023-12-01 18:54:26 +0100

    blog form styles

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogForm extends Component {
   constructor(props) {
@@ -13,8 +14,36 @@ export default class BlogForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_blog[title]", this.state.title);
+    formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+
+    return formData;
+  }
+
   handleSubmit(event) {
-    this.props.handleSuccessfullFormSubmission(this.state);
+    axios
+      .post(
+        "https://jordan.devcamp.space/portfolio/portfolio_blogs",
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.props.handleSuccessfullFormSubmission(
+          response.data.portfolio_blog
+        );
+
+        this.setState({
+          title: "",
+          blog_status: ""
+        });
+      })
+      .catch(error => {
+        console.log("handleSubmit for blog error", error);
+      });
+
     event.preventDefault();
   }
 
@@ -26,24 +55,26 @@ export default class BlogForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          name="title"
-          placeholder="Blog Title"
-          value={this.state.title}
-        />
+      <form onSubmit={this.handleSubmit} className="blog-form-wrapper">
+        <div className="two-column">
+          <input
+            type="text"
+            onChange={this.handleChange}
+            name="title"
+            placeholder="Blog Title"
+            value={this.state.title}
+          />
 
-        <input
-          type="text"
-          onChange={this.handleChange}
-          name="blog_status"
-          placeholder="Blog status"
-          value={this.state.blog_status}
-        />
+          <input
+            type="text"
+            onChange={this.handleChange}
+            name="blog_status"
+            placeholder="Blog status"
+            value={this.state.blog_status}
+          />
+        </div>
 
-        <button>Save</button>
+        <button className="btn">Save</button>
       </form>
     );
   }
